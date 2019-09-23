e77b2906065b251a1fb17f0a9712af131180577d
Tmax033 <43695845+Tmax033@users.noreply.github.com>
2019-09-23 08:30:33 +0530

    viewBooking
    android libraries

--- app/src/main/java/com/example/bookeasy/viewBookings.java
+++ app/src/main/java/com/example/bookeasy/viewBookings.java
@@ -8,8 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
