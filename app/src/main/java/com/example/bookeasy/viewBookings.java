a84c9f231e3bc45df930d44613baf16bb8930199
Tmax033 <43695845+Tmax033@users.noreply.github.com>
2019-09-23 08:30:53 +0530

    Update viewBookings.java

--- app/src/main/java/com/example/bookeasy/viewBookings.java
+++ app/src/main/java/com/example/bookeasy/viewBookings.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.google.firebase.database.DatabaseError;
